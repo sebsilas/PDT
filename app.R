582560ff6d49d14219f539695eb7c3057f37c56e
Seb Silas <sebsilas@sebs-mbp.home>
2020-05-21 17:33:27 +0100

    Basic working version

--- app.R
+++ app.R
@@ -1,31 +1,47 @@
 library(shiny)
 library(psychTestR)
 library(psychTestRCAT)
+library(htmltools)
 
 item.bank <- read.csv("ItemBank.csv")
 
 
-page.return <- function(item, state, ...) {
+show.item <- function(item, state, ...) {
+
+  item_number <- psychTestRCAT::get_item_number(item)
   
-  NAFC_page(label = item[, "label"], 
-            prompt = shiny::tags$button(id = "playTones", 
-                               onclick=sprintf("playTones(%s, %s, %s)", item[, "tone.1"], item[, "tone.2"], item[, "tone.3"])),
-            choices = c("1","2","3")
-            )
+  tones <- sprintf("playTones([%s, %s, %s]);", item["tone.1"], item["tone.2"], item["tone.3"])
+  
+  print(tones)
+  
+  NAFC_page(label = paste0("q", item_number), 
+             prompt = div(includeScript("www/js/Tone.js"),
+                          includeScript("www/js/main.js"),
+                          shiny::tags$script(tones),
+                          shiny::tags$p("Click the odd one out.")),
+             choices = c("1","2","3"),
+            arrange_vertically = FALSE
+             )
   
 }
 
-PDCT.test <- psychTestRCAT::adapt_test(label = "PDCT", 
+timeline <- psychTestR::join(one_button_page("Hello, welcome to the test"),
+        psychTestRCAT::adapt_test(label = "PDCT", 
         item_bank = item.bank,
-        show_item = page.return,
-        stopping_rule = stopping_rule.num_items(n = NULL),
-        opt = adapt_test_options(next_item.criterion = "MLP",
+        show_item = show.item,
+        stopping_rule = stopping_rule.num_items(n = 10),
+        opt = adapt_test_options(next_item.criterion = "MFI",
                                next_item.estimator = "BM", next_item.prior_dist = "norm",
                                  next_item.prior_par = c(0, 1), final_ability.estimator = "BM",
                                  constrain_answers = FALSE, avoid_duplicates = NULL,
                                  cb_control = NULL, cb_group = NULL, eligible_first_items = NULL,
-                                 notify_duration = 5)
-        )
+                                 notify_duration = 5)), final_page("The End!"))
 
-shiny::runApp(PDCT.test)
+
+PDCT.test <- make_test(elts = timeline)
+
+#shiny::runApp(PDCT.test)
+
+
+
 
