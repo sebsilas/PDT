6b9d47f1d78a9a28b077325aceedb4f27b740a14
Seb Silas <sebsilas@sebs-mbp.home>
2020-05-23 06:50:38 +0100

    With (non-working) random answer generation on-the-fly

--- app.R
+++ app.R
@@ -3,18 +3,43 @@ library(psychTestR)
 library(psychTestRCAT)
 library(htmltools)
 
-item.bank <- read.csv("ItemBank.csv")
+item.bank <- read.csv("PDCT_IRT_ItemBank.csv")
+
+# instantiate columns
+item.bank$answer <- NULL
+item.bank$tone.1 <- NULL
+item.bank$tone.2 <- NULL
+item.bank$tone.3 <- NULL
+
+random.tone <- function(row) {
+  
+  # create random position of answer
+  answer <- sample(3,1)
+  
+  row["answer"] <- answer
+  
+  # define position of level tone (1,2 or 3 is the answer)
+  row[paste0("tone.",answer)] <- row["level"]
+  
+  # and the standards
+  standards <- setdiff(c(1,2,3),answer)
+  row[paste0("tone.",standards[1])] <- 330
+  row[paste0("tone.",standards[2])] <- 330
+  
+  return(row)
+  
+}
 
 
 show.item <- function(item, state, ...) {
-
+  
   item_number <- psychTestRCAT::get_item_number(item)
+  
+  item <- random.tone(item)
   
   tones <- sprintf("playTones([%s, %s, %s]);", item["tone.1"], item["tone.2"], item["tone.3"])
   
-  print(tones)
-  
-  NAFC_page(label = paste0("q", item_number), 
+  page <- NAFC_page(label = paste0("q", item_number), 
              prompt = div(includeScript("www/js/Tone.js"),
                           includeScript("www/js/main.js"),
                           shiny::tags$script(tones),
@@ -23,25 +48,47 @@ show.item <- function(item, state, ...) {
             arrange_vertically = FALSE
              )
   
+  return(page)
+  
 }
 
-timeline <- psychTestR::join(one_button_page("Hello, welcome to the test"),
+timeline <- psychTestR::join(
+        one_button_page(shiny::tags$h4("Hello, welcome to the Pitch Discrimination Task!")),
+        one_button_page(div(shiny::tags$p("In this task, on each trial you will hear three tones."),
+                        shiny::tags$p("One will be higher than the other two."),
+                        shiny::tags$p("You must click which tone you think is the odd one out (the highest tone)."))),
+        one_button_page(shiny::tags$h4("Let's begin!")),
+        
+        
         psychTestRCAT::adapt_test(label = "PDCT", 
         item_bank = item.bank,
         show_item = show.item,
         stopping_rule = stopping_rule.num_items(n = 10),
-        opt = adapt_test_options(next_item.criterion = "MFI",
-                               next_item.estimator = "BM", next_item.prior_dist = "norm",
-                                 next_item.prior_par = c(0, 1), final_ability.estimator = "BM",
-                                 constrain_answers = FALSE, avoid_duplicates = NULL,
-                                 cb_control = NULL, cb_group = NULL, eligible_first_items = NULL,
-                                 notify_duration = 5)), final_page("The End!"))
+        opt = adapt_test_options(next_item.criterion = "bOpt",
+                               next_item.estimator = "BM",
+                                final_ability.estimator = "WL",
+                                  eligible_first_items = c(97,98,99)
+                               )),
+        # save results
+        elt_save_results_to_disk(complete = TRUE),
+        # final page
+        final_page("The End!")
+        )
 
 
 PDCT.test <- make_test(elts = timeline)
 
-#shiny::runApp(PDCT.test)
+runApp(PDCT.test)
 
+#rsconnect::deployApp('/Users/sebsilas/Desktop/Melody Production Dissertation/Pitch-Discrimination-Task/task')
 
+# Other Parameters:
+
+# c(next_item.prior_dist = "norm",  
+# next_item.prior_par = c(0, 1), 
+# constrain_answers = FALSE, 
+# avoid_duplicates = NULL,
+# cb_control = NULL, cb_group = NULL,
+# notify_duration = 5)
 
 
