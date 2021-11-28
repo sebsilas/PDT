232444a7f1aa22db854ef10500de0520d9f57434
syntheso <sebsilas@gmail.com>
2021-11-28 17:03:49 +0000

    x

--- R/PDT.R
+++ R/PDT.R
@@ -1,19 +1,25 @@
 
+
 #' Launch the Pitch Discrimination Task (PDT)
 #'
 #' @param num_items
 #' @param with_final_page
+#' @param headphones_page
+#' @param import_musicassessr_js_scripts
 #'
 #' @return
 #' @export
 #'
 #' @examples
-PDT <- function(num_items = 10L, with_final_page = TRUE) {
+PDT <- function(num_items = 10L, with_final_page = TRUE,
+                headphones_page = TRUE, import_musicassessr_js_scripts = TRUE) {
 
   timeline <- psychTestR::new_timeline(psychTestR::join(
 
-    psychTestR::one_button_page(shiny::tags$div(musicassessr::musicassessr_js_scripts(), shiny::tags$h4("Hello, welcome to the Pitch Discrimination Task!"))),
-    musicassessr::test_headphones_page(),
+    psychTestR::one_button_page(shiny::tags$div(
+                                if(import_musicassessr_js_scripts) musicassessr::musicassessr_js_scripts(),
+                                shiny::tags$h4("Hello, welcome to the Pitch Discrimination Task!"))),
+    if(headphones_page) musicassessr::test_headphones_page(),
     psychTestR::one_button_page(shiny::tags$div(shiny::tags$p("In this task, on each trial you will hear three tones."),
                           shiny::tags$p("One will be higher than the other two."),
                           shiny::tags$p("You must click which tone you think is the odd one out (the highest tone)."))),
