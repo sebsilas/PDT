1da56f8e0ce85814e60186acf593a9d073d81123
syntheso <sebsilas@gmail.com>
2022-10-03 09:05:59 +0200

    Updated to use Seb's fork psychTestRCATME

--- R/PDT.R
+++ R/PDT.R
@@ -6,34 +6,38 @@
 #' @param num_items
 #' @param with_final_page
 #' @param headphones_page
+#' @param demo
 #'
 #' @return
 #' @export
 #'
 #' @examples
-PDT <- function(num_items = 10L, with_final_page = TRUE,
-                headphones_page = TRUE) {
+PDT <- function(num_items = 10L,
+                with_final_page = TRUE,
+                headphones_page = TRUE,
+                demo = FALSE) {
 
   timeline <- psychTestR::new_timeline(psychTestR::join(
 
     psychTestR::one_button_page(shiny::tags$div(
                                 shiny::tags$h4("Hello, welcome to the Pitch Discrimination Task!"))),
 
-    if(headphones_page) musicassessr::test_headphones_page(),
+    if(headphones_page) musicassessr::test_headphones_page(concise_wording = TRUE),
 
     psychTestR::one_button_page(shiny::tags$div(shiny::tags$p("In this task, on each trial you will hear three tones."),
                           shiny::tags$p("One will be higher than the other two."),
                           shiny::tags$p("You must click which tone you think is the odd one out (the highest tone)."))),
     psychTestR::one_button_page(shiny::tags$h4("Let's begin!")),
 
-          psychTestRCAT::adapt_test(label = "PDT",
+          psychTestRCATME::adapt_test(label = "PDT",
           item_bank = PDCT_item_bank,
           show_item = show_item,
-          stopping_rule = psychTestRCAT::stopping_rule.num_items(n = num_items),
-          opt = psychTestRCAT::adapt_test_options(next_item.criterion = "bOpt",
+          stopping_rule = psychTestRCATME::stopping_rule.num_items(n = num_items),
+          opt = psychTestRCATME::adapt_test_options(next_item.criterion = "bOpt",
                                  next_item.estimator = "BM",
                                   final_ability.estimator = "WL",
-                                    eligible_first_items = c(289, 292, 295)
+                                    eligible_first_items = c(289, 292, 295),
+                                 demo = demo
                                  )),
           # save results
     psychTestR::elt_save_results_to_disk(complete = FALSE),
@@ -51,6 +55,7 @@ PDT <- function(num_items = 10L, with_final_page = TRUE,
 #' @param admin_password
 #' @param researcher_email
 #' @param num_items
+#' @param demo
 #'
 #' @return
 #' @export
@@ -59,15 +64,16 @@ PDT <- function(num_items = 10L, with_final_page = TRUE,
 PDT_standalone <- function(title = "Pitch Discrimination Task",
                            admin_password = 'demo',
                            researcher_email = 'sebsilas@gmail.com',
-                           num_items = 10L) {
+                           num_items = 10L,
+                           demo = FALSE) {
 
-  timeline <- PDT(num_items = num_items)
+  timeline <- PDT(num_items = num_items, demo = demo)
 
   psychTestR::make_test(elts = timeline,
                         opt = psychTestR::test_options(
                           title = title,
                           admin_password = admin_password,
-                          additional_scripts = musicassessr::musicassessr_js()
+                          additional_scripts = musicassessr::musicassessr_js(record_audio = FALSE)
                         ))
 
 }
@@ -75,7 +81,7 @@ PDT_standalone <- function(title = "Pitch Discrimination Task",
 
 show_item <- function(item, state, ...) {
 
-  item_number <- psychTestRCAT::get_item_number(item)
+  item_number <- psychTestRCATME::get_item_number(item)
 
   tones <- sprintf("playTones([%s, %s, %s]);", item["tone.1"], item["tone.2"], item["tone.3"])
 
@@ -90,3 +96,5 @@ show_item <- function(item, state, ...) {
 
 }
 
+
+# PDT_standalone(demo = TRUE)
